532f4e24bdf53841269e072de9c0c4cf5fe2e956
Rustem Kakimov <ruskakimov@gmail.com>
2022-09-27 21:12:04 +0600

    Document cumulative

--- src/common/math.ts
+++ src/common/math.ts
@@ -2,6 +2,9 @@ export function clamp(value: number, min: number, max: number) {
   return Math.min(max, Math.max(min, value));
 }
 
+/**
+ * Returns a new array of numbers, where array[i] is a sum of [0:i] range in the original array.
+ */
 export function cumulative(array: number[]): number[] {
   let total = 0;
   return array.map((value) => {
