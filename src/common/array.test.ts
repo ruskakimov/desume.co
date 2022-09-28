3779314cb06771979529b50ca8b2b91da5e0c6a2
Rustem Kakimov <ruskakimov@gmail.com>
2022-09-28 20:35:54 +0600

    Test remove

--- src/common/array.test.ts
+++ src/common/array.test.ts
@@ -1,6 +1,6 @@
-import { withInsertedAt } from "./array";
+import { withInsertedAt, withRemovedAt } from "./array";
 
-describe("withInserted", () => {
+describe("withInsertedAt", () => {
   test("inserts a value in last place", () => {
     expect(withInsertedAt([1, 2, 3], 3, 4)).toEqual([1, 2, 3, 4]);
   });
@@ -11,5 +11,19 @@ describe("withInserted", () => {
 
   test("inserts a value in the middle", () => {
     expect(withInsertedAt([1, 2, 3], 1, 42)).toEqual([1, 42, 2, 3]);
+  });
+});
+
+describe("withRemovedAt", () => {
+  test("removes a value in last place", () => {
+    expect(withRemovedAt([1, 2, 3], 2)).toEqual([1, 2]);
+  });
+
+  test("removes a value in first place", () => {
+    expect(withRemovedAt([1, 2, 3], 0)).toEqual([2, 3]);
+  });
+
+  test("removes a value in the middle", () => {
+    expect(withRemovedAt([1, 2, 3], 1)).toEqual([1, 3]);
   });
 });
