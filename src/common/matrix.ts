67592b9c811562c44559f3e03d2e41b8699de6c2
Rustem Kakimov <ruskakimov@gmail.com>
2022-09-28 21:08:17 +0600

    Implement row insertion

--- src/common/matrix.ts
+++ src/common/matrix.ts
@@ -1,23 +1,25 @@
 import { withInsertedAt } from "./array";
 
 /**
- * Returns a new matrix with a column inserted at [index] filled with [value].
+ * Returns a new matrix with a column inserted at [index] filled with [fillValue].
  */
 export function withInsertedColumn<T>(
   matrix: T[][],
   index: number,
-  value: T
+  fillValue: T
 ): T[][] {
-  return matrix.map((row) => withInsertedAt(row, index, value));
+  return matrix.map((row) => withInsertedAt(row, index, fillValue));
 }
 
 /**
- * Returns a new matrix with a row inserted at [index] filled with [value].
+ * Returns a new matrix with a row inserted at [index] filled with [fillValue].
  */
 export function withInsertedRow<T>(
   matrix: T[][],
   index: number,
-  value: T
+  fillValue: T
 ): T[][] {
-  return matrix;
+  const columnCount = matrix[0].length;
+  const newRow = Array(columnCount).fill(fillValue);
+  return withInsertedAt(matrix, index, newRow);
 }
