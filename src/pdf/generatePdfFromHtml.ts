1df2c7e78f1738594f1b4662b85aa66c4c0ebc70
Rustem Kakimov <ruskakimov@gmail.com>
2022-10-02 18:55:07 +0600

    Extract render functions

--- src/pdf/generatePdfFromHtml.ts
+++ src/pdf/generatePdfFromHtml.ts
@@ -1,11 +1,8 @@
 import jsPDF from "jspdf";
 import { Box } from "../common/box";
 import { a4SizeInPoints } from "../common/constants/sizes";
+import { Coord } from "../common/types";
 import { PDF } from "./types";
-
-function boxFromDomRect({ x, y, width, height }: DOMRect): Box {
-  return new Box(x, y, width, height);
-}
 
 /**
  * Generates a single-page PDF document from an HTML element.
@@ -24,31 +21,50 @@ export function generatePdfFromHtml(pageElement: HTMLElement): PDF {
 
   Array.from(pageElement.children).forEach((cell) => {
     const cellBox = pdfBoxOf(cell);
-
-    doc.rect(
-      cellBox.topLeft.x,
-      cellBox.topLeft.y,
-      cellBox.size.width,
-      cellBox.size.height
-    );
+    renderBox(doc, cellBox);
 
     Array.from(cell.children).forEach((el) => {
-      const pdfBox = pdfBoxOf(el);
+      const elBox = pdfBoxOf(el);
 
       // We assume to only receive px values here
       const fontSizePx = parseFloat(
         getComputedStyle(el).getPropertyValue("font-size")
       );
 
-      const { x, y } = pdfBox.topLeft;
-      const { width, height } = pdfBox.size;
-
-      doc
-        .rect(x, y, width, height)
-        .setFontSize(fontSizePx * pdfScalar)
-        .text(el.textContent ?? "", x, y, { baseline: "top" });
+      renderBox(doc, elBox);
+      renderText(doc, elBox.topLeft, el.textContent ?? "", {
+        fontSizePt: fontSizePx * pdfScalar,
+      });
     });
   });
 
   return new PDF(doc);
+}
+
+function boxFromDomRect({ x, y, width, height }: DOMRect): Box {
+  return new Box(x, y, width, height);
+}
+
+function renderBox(doc: jsPDF, pdfBox: Box) {
+  doc.rect(
+    pdfBox.topLeft.x,
+    pdfBox.topLeft.y,
+    pdfBox.size.width,
+    pdfBox.size.height
+  );
+}
+
+interface TextOptions {
+  fontSizePt: number;
+}
+
+function renderText(
+  doc: jsPDF,
+  topLeft: Coord,
+  text: string,
+  options: TextOptions
+) {
+  doc
+    .setFontSize(options.fontSizePt)
+    .text(text, topLeft.x, topLeft.y, { baseline: "top" });
 }
