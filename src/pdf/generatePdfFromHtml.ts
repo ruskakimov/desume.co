d7d39759fff15404e1e50ac3ff1129b5e5d916bb
Rustem Kakimov <ruskakimov@gmail.com>
2022-10-02 18:37:30 +0600

    Use box class

--- src/pdf/generatePdfFromHtml.ts
+++ src/pdf/generatePdfFromHtml.ts
@@ -1,6 +1,11 @@
 import jsPDF from "jspdf";
+import { Box } from "../common/box";
 import { a4SizeInPoints } from "../common/constants/sizes";
 import { PDF } from "./types";
+
+function boxFromDomRect({ x, y, width, height }: DOMRect): Box {
+  return new Box(x, y, width, height);
+}
 
 /**
  * Generates a single-page PDF document from an HTML element.
@@ -8,29 +13,31 @@ import { PDF } from "./types";
 export function generatePdfFromHtml(pageElement: HTMLElement): PDF {
   const doc = new jsPDF({ format: "a4", unit: "pt" });
 
-  const rootRect = pageElement.getBoundingClientRect();
-  const scalar = a4SizeInPoints.width / rootRect.width;
+  const pageBox = boxFromDomRect(pageElement.getBoundingClientRect());
+  const pdfScalar = a4SizeInPoints.width / pageBox.size.width;
+
   const cells = Array.from(pageElement.children);
 
   cells.forEach((cell) => {
     const elements = Array.from(cell.children);
 
     elements.forEach((el) => {
-      const { top, left, bottom, right, width, height } =
-        el.getBoundingClientRect();
-
-      const x = (left - rootRect.left) * scalar;
-      const yTop = (top - rootRect.top) * scalar;
+      const pdfBox = boxFromDomRect(el.getBoundingClientRect())
+        .relativeTo(pageBox)
+        .scaleBy(pdfScalar);
 
       // We assume to only receive px values here
       const fontSizePx = parseFloat(
         getComputedStyle(el).getPropertyValue("font-size")
       );
 
-      doc.rect(x, yTop, width, height);
+      const { x, y } = pdfBox.topLeft;
+      const { width, height } = pdfBox.size;
+
       doc
-        .setFontSize(fontSizePx * scalar)
-        .text(el.textContent ?? "", x, yTop, { baseline: "top" });
+        .rect(x, y, width, height)
+        .setFontSize(fontSizePx * pdfScalar)
+        .text(el.textContent ?? "", x, y, { baseline: "top" });
     });
   });
 
