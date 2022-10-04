0affa1d036c6d61810f60dc8c1e5c9a97155f078
Rustem Kakimov <ruskakimov@gmail.com>
2022-10-04 19:14:12 +0600

    Test Times props calc

--- src/pdf/fontProperties.test.ts
+++ src/pdf/fontProperties.test.ts
@@ -22,11 +22,20 @@ describe("getFontProperties", () => {
       normalLineHeightFactor: 1.15,
       baselineRatio: 0.8465,
     };
-
     const calcProperties = await page.evaluate(async () => {
       return (window as any)["getFontProperties"]("Arial");
     });
-
     expect(calcProperties).toEqual(arialFontProperties);
+  });
+
+  test("calculates Times properties", async () => {
+    const timesFontProperties: FontProperties = {
+      normalLineHeightFactor: 1.15,
+      baselineRatio: 0.825,
+    };
+    const calcProperties = await page.evaluate(async () => {
+      return (window as any)["getFontProperties"]("Times");
+    });
+    expect(calcProperties).toEqual(timesFontProperties);
   });
 });
