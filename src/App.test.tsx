1bcac2b1cbe9ebfebca4c66fd883bf469356d75d
Rustem Kakimov <ruskakimov@gmail.com>
2022-10-04 19:00:57 +0600

    Remove useless test

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders save button", () => {
-  render(<App />);
-  const saveElement = screen.getByText(/save/i);
-  expect(saveElement).toBeInTheDocument();
-});
