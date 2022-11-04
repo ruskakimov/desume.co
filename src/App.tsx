0fc1c31ad28fd3849de6b5def09dfa9160e93475
Rustem Kakimov <ruskakimov@gmail.com>
2022-11-04 20:26:31 +0600

    Add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import DocumentGrid from "./DocumentGrid";
+import logo from "./text-logo.svg";
 
 function App() {
   return (
     <div className="h-screen w-screen flex flex-col">
-      <header className="h-12 bg-gray-1 border-b border-gray-3"></header>
+      <header className="h-12 bg-gray-1 border-b border-gray-3">
+        <img className="h-full" src={logo} />
+      </header>
       <main className="grow flex flex-row">
         <section className="shrink-0 w-40 bg-gray-1 border-r border-gray-3"></section>
         <section className="grow overflow-auto bg-gray-2">
