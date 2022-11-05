424a3e772cdb59f6d892a8d56861ad671e73ff6e
Rustem Kakimov <ruskakimov@gmail.com>
2022-11-05 12:46:37 +0600

    Align logo with cards

--- src/editor/Editor.tsx
+++ src/editor/Editor.tsx
@@ -7,7 +7,7 @@ export default function Editor() {
   return (
     <div className="absolute inset-0 flex flex-col overflow-hidden">
       <header className="h-12 shrink-0 bg-gray-1 border-b border-gray-3 flex justify-between items-center">
-        <img className="h-full" src={logo} />
+        <img className="h-full ml-2" src={logo} />
         <PrimaryButton>Export PDF</PrimaryButton>
       </header>
       <main className="grow min-h-0 flex flex-row">
