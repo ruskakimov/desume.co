36afbc014077692931489d1d72f5f18088e6f5eb
Rustem Kakimov <ruskakimov@gmail.com>
2022-11-05 17:29:17 +0600

    Extract document area shell

--- src/editor/DocumentArea.tsx
+++ src/editor/DocumentArea.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../app/store";
 import { a4SizeInPoints } from "../common/constants/sizes";
@@ -9,12 +10,25 @@ const scale = 1;
 const documentWidth = a4SizeInPoints.width * scale;
 const documentHeight = a4SizeInPoints.height * scale;
 
-export default function DocumentArea({}) {
-  const pageMargins = useSelector(
-    (state: RootState) => state.document.pageMargins
-  );
+export default function DocumentArea() {
   const docComponents = useSelector(
     (state: RootState) => state.document.components
+  );
+
+  return (
+    <DocumentAreaShell>
+      <Page pageWidth={documentWidth} pageHeight={documentHeight}>
+        {docComponents.map((component, index) => (
+          <h1 key={index}>{component.text}</h1>
+        ))}
+      </Page>
+    </DocumentAreaShell>
+  );
+}
+
+function DocumentAreaShell(props: { children: React.ReactNode }) {
+  const pageMargins = useSelector(
+    (state: RootState) => state.document.pageMargins
   );
   const dispatch = useDispatch();
 
@@ -23,13 +37,7 @@ export default function DocumentArea({}) {
   return (
     <div className="h-full w-full relative">
       <div className="max-h-full overflow-scroll">
-        <div className="py-32">
-          <Page pageWidth={documentWidth} pageHeight={documentHeight}>
-            {docComponents.map((component, index) => (
-              <h1 key={index}>{component.text}</h1>
-            ))}
-          </Page>
-        </div>
+        <div className="py-32">{props.children}</div>
       </div>
 
       <div className="h-full absolute top-0 left-1/2 -translate-x-1/2 pointer-events-none">
