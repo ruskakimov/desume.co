929f8284927dab815a2c7c18f0b3effe03a9c6e7
Rustem Kakimov <ruskakimov@gmail.com>
2022-11-05 18:57:45 +0600

    Adjust margin

--- src/editor/ComponentCard.tsx
+++ src/editor/ComponentCard.tsx
@@ -4,7 +4,7 @@ interface ComponentCardProps {
 
 export default function ComponentCard({ name }: ComponentCardProps) {
   return (
-    <div className="h-9 mb-4 border border-gray-3 rounded flex items-center text-sm text-gray-5 select-none hover:shadow-lg transition cursor-grab">
+    <div className="h-9 mb-3 border border-gray-3 rounded flex items-center text-sm text-gray-5 select-none hover:shadow-lg transition cursor-grab">
       <div className="h-5 w-5 mx-2 bg-gray-2 rounded-sm"></div>
       {name}
     </div>
