65540d564f5ffbd638299fd7c99ac23b08fe51fb
Rustem Kakimov <ruskakimov@gmail.com>
2022-11-06 15:55:49 +0600

    More padding on right

--- src/editor/MarkdownArea.tsx
+++ src/editor/MarkdownArea.tsx
@@ -2,7 +2,7 @@ export default function MarkdownArea() {
   return (
     <div className="h-full">
       <textarea
-        className="h-full w-full px-4 py-8 text-gray-5 text-lg border-none focus:border-none focus:ring-0 resize-none placeholder:text-gray-3"
+        className="h-full w-full pl-4 pr-8 py-8 text-gray-5 text-lg border-none focus:border-none focus:ring-0 resize-none placeholder:text-gray-3"
         placeholder="Write here..."
       />
     </div>
