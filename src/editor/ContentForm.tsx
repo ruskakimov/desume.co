bda8d201d59a7cb62579995822f2c815ea4b30f0
Rustem Kakimov <ruskakimov@gmail.com>
2022-11-12 18:16:14 +0600

    Date pickers

--- src/editor/ContentForm.tsx
+++ src/editor/ContentForm.tsx
@@ -94,7 +94,7 @@ export default function ContentForm() {
                     htmlFor="first-name"
                     className="block text-sm font-medium text-gray-700"
                   >
-                    Company
+                    Company name
                   </label>
                   <input
                     type="text"
@@ -110,7 +110,7 @@ export default function ContentForm() {
                     htmlFor="company-website"
                     className="block text-sm font-medium text-gray-700"
                   >
-                    Website
+                    Company website
                   </label>
                   <div className="mt-1 flex rounded-md shadow-sm">
                     <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-gray-500 sm:text-sm">
@@ -124,113 +124,91 @@ export default function ContentForm() {
                     />
                   </div>
                 </div>
-              </div>
 
-              <div className="grid grid-cols-3 gap-6">
-                <div className="col-span-3 sm:col-span-2">
+                <div className="col-span-6">
                   <label
-                    htmlFor="company-website"
+                    htmlFor="first-name"
                     className="block text-sm font-medium text-gray-700"
                   >
-                    Username
+                    Job title
                   </label>
-                  <div className="mt-1 flex rounded-md shadow-sm">
-                    <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-gray-500 sm:text-sm">
-                      workcation.com/
-                    </span>
-                    <input
-                      type="text"
-                      name="username"
-                      id="username"
-                      autoComplete="username"
-                      className="block w-full min-w-0 flex-grow rounded-none rounded-r-md border-gray-300 focus:border-gray-500 focus:ring-gray-500 sm:text-sm"
-                    />
-                  </div>
+                  <input
+                    type="text"
+                    name="first-name"
+                    id="first-name"
+                    autoComplete="given-name"
+                    className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-gray-500 focus:outline-none focus:ring-gray-500 sm:text-sm"
+                  />
                 </div>
 
-                <div className="col-span-3">
+                <div className="col-span-6 sm:col-span-3">
                   <label
-                    htmlFor="about"
+                    htmlFor="first-name"
                     className="block text-sm font-medium text-gray-700"
                   >
-                    About
+                    Start
                   </label>
-                  <div className="mt-1">
-                    <textarea
-                      id="about"
-                      name="about"
-                      rows={3}
-                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-gray-500 focus:ring-gray-500 sm:text-sm"
-                      placeholder="you@example.com"
-                      defaultValue={""}
-                    />
-                  </div>
-                  <p className="mt-2 text-sm text-gray-500">
-                    Brief description for your profile. URLs are hyperlinked.
-                  </p>
-                </div>
-
-                <div className="col-span-3">
-                  <label className="block text-sm font-medium text-gray-700">
-                    Photo
-                  </label>
-                  <div className="mt-1 flex items-center">
-                    <span className="inline-block h-12 w-12 overflow-hidden rounded-full bg-gray-100">
-                      <svg
-                        className="h-full w-full text-gray-300"
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
+                  <div className="mt-1 flex -space-x-px">
+                    <div className="w-1/2 min-w-0 flex-1">
+                      <label htmlFor="month" className="sr-only">
+                        Month
+                      </label>
+                      <select
+                        id="month"
+                        name="month"
+                        className="relative block w-full rounded-none rounded-tl-md rounded-bl-md border-gray-300 bg-transparent focus:z-10 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                       >
-                        <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                      </svg>
-                    </span>
-                    <button
-                      type="button"
-                      className="ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
-                    >
-                      Change
-                    </button>
+                        <option>January</option>
+                        <option>February</option>
+                      </select>
+                    </div>
+                    <div className="min-w-0 flex-1">
+                      <label htmlFor="card-cvc" className="sr-only">
+                        Year
+                      </label>
+                      <input
+                        type="text"
+                        name="card-cvc"
+                        id="card-cvc"
+                        className="relative block w-full rounded-none rounded-br-md rounded-tr-md border-gray-300 bg-transparent focus:z-10 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                        placeholder="Year"
+                      />
+                    </div>
                   </div>
                 </div>
 
-                <div className="col-span-3">
-                  <label className="block text-sm font-medium text-gray-700">
-                    Cover photo
+                <div className="col-span-6 sm:col-span-3">
+                  <label
+                    htmlFor="first-name"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    End
                   </label>
-                  <div className="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
-                    <div className="space-y-1 text-center">
-                      <svg
-                        className="mx-auto h-12 w-12 text-gray-400"
-                        stroke="currentColor"
-                        fill="none"
-                        viewBox="0 0 48 48"
-                        aria-hidden="true"
+                  <div className="mt-1 flex -space-x-px">
+                    <div className="w-1/2 min-w-0 flex-1">
+                      <label htmlFor="month" className="sr-only">
+                        Month
+                      </label>
+                      <select
+                        id="month"
+                        name="month"
+                        className="relative block w-full rounded-none rounded-tl-md rounded-bl-md border-gray-300 bg-transparent focus:z-10 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                       >
-                        <path
-                          d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                          strokeWidth={2}
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        />
-                      </svg>
-                      <div className="flex text-sm text-gray-600">
-                        <label
-                          htmlFor="file-upload"
-                          className="relative cursor-pointer rounded-md bg-white font-medium text-gray-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-gray-500 focus-within:ring-offset-2 hover:text-gray-500"
-                        >
-                          <span>Upload a file</span>
-                          <input
-                            id="file-upload"
-                            name="file-upload"
-                            type="file"
-                            className="sr-only"
-                          />
-                        </label>
-                        <p className="pl-1">or drag and drop</p>
-                      </div>
-                      <p className="text-xs text-gray-500">
-                        PNG, JPG, GIF up to 10MB
-                      </p>
+                        <option>January</option>
+                        <option>February</option>
+                      </select>
+                    </div>
+                    <div className="min-w-0 flex-1">
+                      <label htmlFor="card-cvc" className="sr-only">
+                        Year
+                      </label>
+                      <input
+                        type="text"
+                        name="card-cvc"
+                        id="card-cvc"
+                        className="relative block w-full rounded-none rounded-br-md rounded-tr-md border-gray-300 bg-transparent focus:z-10 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                        placeholder="Year"
+                      />
                     </div>
                   </div>
                 </div>
