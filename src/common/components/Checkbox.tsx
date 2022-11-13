6b058ce16ac5732dfe6f8247271b67265e82f814
Rustem Kakimov <ruskakimov@gmail.com>
2022-11-13 14:20:27 +0600

    Checkbox props

--- src/common/components/Checkbox.tsx
+++ src/common/components/Checkbox.tsx
@@ -1,10 +1,14 @@
-export default function Checkbox() {
+interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  id?: string;
+}
+
+export default function Checkbox({ id, ...props }: CheckboxProps) {
   return (
     <input
-      // id="comments"
-      // name="comments"
+      id={id}
+      {...props}
       type="checkbox"
-      className="h-4 w-4 mr-3 rounded border-gray-300 text-gray-600 focus:ring-gray-500"
+      className="h-4 w-4 mr-3 rounded border-gray-300 text-gray-600 disabled:text-gray-300 focus:ring-gray-500"
     />
   );
 }
