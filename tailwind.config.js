4c02577be33013a6ddcb75bda386c1cd2f56e3f1
Rustem Kakimov <ruskakimov@gmail.com>
2022-12-09 20:24:10 +0600

    Uninstall tailwind typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,6 @@ module.exports = {
     fontFamily: {
       body: ["Roboto", "sans-serif"],
     },
-    // colors: {
-    //   white: "#FFFFFF",
-    //   blue: "#0060EF",
-    //   gray: {
-    //     1: "#FCFCFC",
-    //     2: "#E6E6E6",
-    //     3: "#CFCFCF",
-    //     4: "#828282",
-    //     5: "#333333",
-    //   },
-    // },
   },
-  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/forms")],
 };
