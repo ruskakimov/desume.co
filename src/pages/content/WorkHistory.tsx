2fa9bef21d37ca29eb5453d1516d88f71c1dbb11
Rustem Kakimov <ruskakimov@gmail.com>
2022-12-09 21:06:58 +0600

    Extract work history card

--- src/pages/content/WorkHistory.tsx
+++ src/pages/content/WorkHistory.tsx
@@ -5,6 +5,7 @@ import TextField from "../../common/components/fields/TextField";
 import WebsiteField from "../../common/components/fields/WebsiteField";
 import PrimaryButton from "../../common/components/PrimaryButton";
 import { WorkExperience } from "../../common/interfaces/resume";
+import WorkHistoryCard from "./WorkHistoryCard";
 
 interface WorkHistoryProps {
   experiences: WorkExperience[];
@@ -28,28 +29,7 @@ const WorkHistory: React.FC<WorkHistoryProps> = ({ experiences }) => {
               <Checkbox />
             </div>
 
-            <div className="border sm:overflow-hidden sm:rounded-md">
-              <div className="bg-white">
-                <div className="grid grid-cols-3">
-                  <div className="p-4 flex flex-col border-r">
-                    <span className="font-medium text-gray-900">
-                      {experience.companyName}
-                    </span>
-                    <span className="font-light text-gray-900">
-                      {experience.jobTitle}
-                    </span>
-                  </div>
-
-                  <div className="col-span-2 p-4 text-sm text-gray-500">
-                    <ul className="list-disc list-inside">
-                      {experience.bulletPoints.map((text) => (
-                        <li className="truncate leading-6">{text}</li>
-                      ))}
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <WorkHistoryCard experience={experience} />
           </div>
         ))}
       </Card>
---
+++ src/pages/content/WorkHistoryCard.tsx
@@ -0,0 +1,34 @@
+import { WorkExperience } from "../../common/interfaces/resume";
+
+interface WorkHistoryCardProps {
+  experience: WorkExperience;
+}
+
+const WorkHistoryCard: React.FC<WorkHistoryCardProps> = ({ experience }) => {
+  return (
+    <div className="border sm:overflow-hidden sm:rounded-md">
+      <div className="bg-white">
+        <div className="grid grid-cols-3">
+          <div className="p-4 flex flex-col border-r">
+            <span className="font-medium text-gray-900">
+              {experience.companyName}
+            </span>
+            <span className="font-light text-gray-900">
+              {experience.jobTitle}
+            </span>
+          </div>
+
+          <div className="col-span-2 p-4 text-sm text-gray-500">
+            <ul className="list-disc list-inside">
+              {experience.bulletPoints.map((text) => (
+                <li className="truncate leading-6">{text}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WorkHistoryCard;
