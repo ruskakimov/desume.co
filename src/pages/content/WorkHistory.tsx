3fdb6378688e7fa218ea1276d8f4e4c7888ca459
Rustem Kakimov <ruskakimov@gmail.com>
2022-12-09 20:45:28 +0600

    Display work experience bullets

--- src/pages/content/WorkHistory.tsx
+++ src/pages/content/WorkHistory.tsx
@@ -23,13 +23,25 @@ const WorkHistory: React.FC<WorkHistoryProps> = ({ experiences }) => {
 
         {experiences.map((experience) => (
           <div className="border sm:overflow-hidden sm:rounded-md">
-            <div className="space-y-6 bg-white py-6 px-4 sm:p-6">
-              <span className="font-medium text-gray-900">
-                {experience.companyName}
-              </span>{" "}
-              <span className="font-light text-gray-900">
-                {experience.jobTitle}
-              </span>
+            <div className="bg-white">
+              <div className="grid grid-cols-3">
+                <div className="p-4 flex flex-col border-r">
+                  <span className="font-medium text-gray-900">
+                    {experience.companyName}
+                  </span>
+                  <span className="font-light text-gray-900">
+                    {experience.jobTitle}
+                  </span>
+                </div>
+
+                <div className="col-span-2 p-4 text-sm text-gray-500">
+                  <ul className="list-disc list-inside">
+                    {experience.bulletPoints.map((text) => (
+                      <li className="truncate leading-6">{text}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
         ))}
