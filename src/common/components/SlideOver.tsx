6d98f4816c03b344190c3ab45107ff7691b93cb9
Rustem Kakimov <ruskakimov@gmail.com>
2022-12-12 22:50:27 +0600

    Change slide over overlay color

--- src/common/components/SlideOver.tsx
+++ src/common/components/SlideOver.tsx
@@ -31,7 +31,7 @@ const SlideOver: React.FC<SlideOverProps> = ({
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-black bg-opacity-70 transition-opacity" />
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
         </Transition.Child>
 
         <div className="fixed inset-0" />
