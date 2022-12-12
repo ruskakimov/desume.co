6ccdee0db49e31e16cf34fe9b655c05017ac95f6
Rustem Kakimov <ruskakimov@gmail.com>
2022-12-12 22:35:09 +0600

    Refactor with secondary button component

--- src/common/components/ConfirmationDialog.tsx
+++ src/common/components/ConfirmationDialog.tsx
@@ -1,6 +1,7 @@
 import { Fragment, useRef } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
+import SecondaryButton from "./SecondaryButton";
 
 export interface ConfirmationDialogProps {
   isOpen: boolean;
@@ -78,14 +79,13 @@ const ConfirmationDialog: React.FC<ConfirmationDialogProps> = ({
                   >
                     {action}
                   </button>
-                  <button
-                    type="button"
-                    className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:w-auto sm:text-sm"
+                  <SecondaryButton
+                    className="mt-3 sm:mt-0 w-full sm:w-auto text-base sm:text-sm"
                     onClick={() => onCancel()}
                     ref={cancelButtonRef}
                   >
                     Cancel
-                  </button>
+                  </SecondaryButton>
                 </div>
               </Dialog.Panel>
             </Transition.Child>
--- src/common/components/SecondaryButton.tsx
+++ src/common/components/SecondaryButton.tsx
@@ -1,18 +1,27 @@
+import classNames from "classnames";
+import React from "react";
+
 interface SecondaryButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-export default function SecondaryButton({
-  children,
-  ...props
-}: SecondaryButtonProps) {
+const SecondaryButton = React.forwardRef<
+  HTMLButtonElement,
+  SecondaryButtonProps
+>(({ children, ...props }, ref) => {
   return (
     <button
       {...props}
-      className="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 no-mouse-focus-ring"
+      ref={ref}
+      className={classNames(
+        "inline-flex justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 no-mouse-focus-ring",
+        props.className
+      )}
     >
       {children}
     </button>
   );
-}
+});
+
+export default SecondaryButton;
