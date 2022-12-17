a4698826edea8acd6302b713eda6d1d01796b9d5
Rustem Kakimov <ruskakimov@gmail.com>
2022-12-17 16:59:48 +0600

    Style social button

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
-import logo from "../../assets/logo.svg";
+import SecondaryButton from "../../common/components/SecondaryButton";
 import LoginForm from "./LoginForm";
 import SignupForm from "./SignupForm";
+
+import logo from "../../assets/logo.svg";
+import googleLogo from "../../assets/google-logo.svg";
 
 const LoginPage: React.FC = () => {
   // TODO: Use /sign-in /sign-up routes instead
@@ -19,11 +22,7 @@ const LoginPage: React.FC = () => {
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <div className="flex flex-col gap-3">
-              <SocialButton />
-              <SocialButton />
-              <SocialButton />
-            </div>
+            <SocialButton icon={googleLogo} label="Continue with Google" />
 
             <OrSeparator />
 
@@ -68,23 +67,15 @@ const OrSeparator: React.FC = () => {
   );
 };
 
-const SocialButton: React.FC = () => {
+const SocialButton: React.FC<{ icon: string; label?: string }> = ({
+  icon,
+  label,
+}) => {
   return (
-    <button className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50">
-      <svg
-        className="h-5 w-5"
-        aria-hidden="true"
-        fill="currentColor"
-        viewBox="0 0 20 20"
-      >
-        <path
-          fillRule="evenodd"
-          d="M20 10c0-5.523-4.477-10-10-10S0 4.477 0 10c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V10h2.54V7.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V10h2.773l-.443 2.89h-2.33v6.988C16.343 19.128 20 14.991 20 10z"
-          clipRule="evenodd"
-        />
-      </svg>
-      <span className="ml-2">Continue with Facebook</span>
-    </button>
+    <SecondaryButton className="w-full text-gray-500">
+      <img className="h-5 w-5" src={icon} />
+      {label !== undefined && <span className="ml-2">{label}</span>}
+    </SecondaryButton>
   );
 };
 
