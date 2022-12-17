87138bcad35943015c555a6ea5d97de4e04350fb
Rustem Kakimov <ruskakimov@gmail.com>
2022-12-17 15:50:01 +0600

    Change title

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -12,6 +12,9 @@ const LoginPage: React.FC = () => {
       <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <img className="mx-auto h-12 w-auto" src={logo} alt="PDFEGG" />
+          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-700">
+            {isSignup ? "Create a new account" : "Sign in to your account"}
+          </h2>
         </div>
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
