4ba7617c52afca892ee72bb3d4a4121fca0b52c5
Rustem Kakimov <ruskakimov@gmail.com>
2022-12-30 22:29:39 +0600

    Add button disabled state

--- src/common/components/PrimaryButton.tsx
+++ src/common/components/PrimaryButton.tsx
@@ -15,7 +15,7 @@ const PrimaryButton = React.forwardRef<HTMLButtonElement, PrimaryButtonProps>(
         ref={ref}
         className={twMerge(
           classNames(
-            "inline-flex justify-center rounded-md border border-transparent bg-gray-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 no-mouse-focus-ring",
+            "inline-flex justify-center rounded-md border border-transparent bg-gray-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 no-mouse-focus-ring disabled:opacity-50 disabled:pointer-events-none",
             props.className
           )
         )}
