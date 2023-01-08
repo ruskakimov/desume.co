60190686f0a45abf5c5b8423245db52bb009da7f
Rustem Kakimov <ruskakimov@gmail.com>
2023-01-08 14:49:21 +0600

    Skill group checkbox

--- src/pages/content/skills/SkillsSection.tsx
+++ src/pages/content/skills/SkillsSection.tsx
@@ -1,5 +1,6 @@
 import { WrenchScrewdriverIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
+import Checkbox from "../../../common/components/Checkbox";
 import EmptyStateAddButton from "../../../common/components/EmptyStateAddButton";
 import { BulletPoint } from "../../../common/interfaces/resume";
 import SortableBulletList from "../components/SortableBulletList";
@@ -18,19 +19,25 @@ const SkillsSection: React.FC = () => {
       </div>
 
       <div className="mt-6 grid grid-cols-3 gap-4">
-        {Array(1)
-          .fill(null)
-          .map(() => (
-            <div className="rounded-md border border-gray-300">
-              <div className="rounded-t-md h-14 px-4 flex flex-row items-center border-b border-gray-300 bg-gray-50">
-                Languages
+        {["Proficient", "Experienced", "Familiar"].map((label) => (
+          <div className="rounded-md border border-gray-300">
+            <div className="rounded-t-md h-14 px-2 flex flex-row gap-2 items-center border-b border-gray-300 bg-gray-50">
+              <div className="mx-2 h-6 flex items-center">
+                <Checkbox
+                  checked={true}
+                  // onChange={(e) => {
+                  //   onChange({ ...experience, included: e.target.checked });
+                  // }}
+                />
               </div>
-              <SortableBulletList
-                bullets={bullets}
-                onChange={(bullets) => setBullets(bullets)}
-              />
+              <span className="font-medium text-gray-900">{label}</span>
             </div>
-          ))}
+            <SortableBulletList
+              bullets={bullets}
+              onChange={(bullets) => setBullets(bullets)}
+            />
+          </div>
+        ))}
 
         <EmptyStateAddButton
           Icon={WrenchScrewdriverIcon}
