19ba5c2193b70391c534004197f2110895f70e36
Rustem Kakimov <ruskakimov@gmail.com>
2023-01-08 23:47:21 +0600

    Fit card to items

--- src/pages/content/skills/SkillsSection.tsx
+++ src/pages/content/skills/SkillsSection.tsx
@@ -43,53 +43,55 @@ const SkillsSection: React.FC = () => {
           };
 
           return (
-            <div className="rounded-md border border-gray-300">
-              <div className="rounded-t-md h-14 px-2 flex flex-row gap-2 items-center border-b border-gray-300 bg-gray-50">
-                <div className="mx-2 h-6 flex items-center">
-                  <Checkbox
-                    checked={true}
-                    onChange={(e) =>
-                      updateSkillGroup({
-                        ...skillGroup,
-                        included: e.target.checked,
-                      })
-                    }
-                  />
-                </div>
-                <span className="font-medium text-gray-900">
-                  {skillGroup.groupName}
-                </span>
+            <div>
+              <div className="rounded-md border border-gray-300">
+                <div className="rounded-t-md h-14 px-2 flex flex-row gap-2 items-center border-b border-gray-300 bg-gray-50">
+                  <div className="mx-2 h-6 flex items-center">
+                    <Checkbox
+                      checked={true}
+                      onChange={(e) =>
+                        updateSkillGroup({
+                          ...skillGroup,
+                          included: e.target.checked,
+                        })
+                      }
+                    />
+                  </div>
+                  <span className="font-medium text-gray-900">
+                    {skillGroup.groupName}
+                  </span>
 
-                <button
-                  type="button"
-                  className="ml-auto mr-2 flex-shrink-0 inline-flex h-8 w-8 items-center justify-center rounded-full text-gray-400 hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 no-mouse-focus-ring"
-                  onClick={() => {
-                    openEditSkillGroupPanel(skillGroup)
-                      .then((skillGroup) => {
-                        if (skillGroup) {
-                          updateSkillGroup(skillGroup);
-                        } else {
-                          setSkillGroups(withRemovedAt(skillGroups, index));
-                        }
-                      })
-                      .catch((e) => {
-                        if (e !== userCancelReason) console.error(e);
-                      });
-                  }}
-                >
-                  <span className="sr-only">Edit skill group</span>
-                  <PencilIcon className="h-5 w-5" />
-                </button>
+                  <button
+                    type="button"
+                    className="ml-auto mr-2 flex-shrink-0 inline-flex h-8 w-8 items-center justify-center rounded-full text-gray-400 hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 no-mouse-focus-ring"
+                    onClick={() => {
+                      openEditSkillGroupPanel(skillGroup)
+                        .then((skillGroup) => {
+                          if (skillGroup) {
+                            updateSkillGroup(skillGroup);
+                          } else {
+                            setSkillGroups(withRemovedAt(skillGroups, index));
+                          }
+                        })
+                        .catch((e) => {
+                          if (e !== userCancelReason) console.error(e);
+                        });
+                    }}
+                  >
+                    <span className="sr-only">Edit skill group</span>
+                    <PencilIcon className="h-5 w-5" />
+                  </button>
+                </div>
+                <SortableBulletList
+                  bullets={skillGroup.skills}
+                  onChange={(skills) =>
+                    updateSkillGroup({
+                      ...skillGroup,
+                      skills,
+                    })
+                  }
+                />
               </div>
-              <SortableBulletList
-                bullets={skillGroup.skills}
-                onChange={(skills) =>
-                  updateSkillGroup({
-                    ...skillGroup,
-                    skills,
-                  })
-                }
-              />
             </div>
           );
         })}
