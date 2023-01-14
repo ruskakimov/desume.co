212fa14f7fc139f8c9c82d08dce598197b46f54d
Rustem Kakimov <ruskakimov@gmail.com>
2023-01-14 16:07:31 +0600

    Sortable section item

---
+++ src/pages/content/components/SortableSectionItem.tsx
@@ -0,0 +1,47 @@
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import classNames from "classnames";
+import React from "react";
+
+const SortableSectionItem: React.FC<{
+  id: string;
+  children: React.ReactNode;
+}> = (props) => {
+  const {
+    isDragging,
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+  } = useSortable({ id: props.id });
+
+  const style = {
+    transform: CSS.Translate.toString(transform),
+    transition,
+  };
+
+  return (
+    <div
+      ref={setNodeRef}
+      className={classNames("transition-shadow relative cursor-auto", {
+        "z-10 shadow-lg bg-white": isDragging,
+      })}
+      style={style}
+      {...attributes}
+    >
+      <div
+        className={classNames(
+          "absolute inset-y-0 w-10 right-0 cursor-grab touch-none",
+          {
+            "cursor-grabbing": isDragging,
+          }
+        )}
+        {...listeners}
+      />
+      {props.children}
+    </div>
+  );
+};
+
+export default SortableSectionItem;
