d4034861ab2a91da5485980d3d2fd2e6f2d71b92
Rustem Kakimov <ruskakimov@gmail.com>
2023-01-14 15:19:56 +0600

    Improve id generation

--- src/common/functions/ids.ts
+++ src/common/functions/ids.ts
@@ -1,8 +1,15 @@
 export function generateId(): string {
-  return new Date().getTime().toString();
+  const timestamp = new Date().getTime().toString();
+  const randomHex = Math.random().toString(16).slice(2);
+  return `${timestamp}-${randomHex}`;
 }
 
+/**
+ * Generates multiple ids at once without collisions.
+ */
 export function generateIds(count: number): string[] {
   const prefix = generateId();
-  return new Array(count).fill(prefix).map((str, index) => `${str}-${index}`);
+  return new Array(count)
+    .fill(prefix)
+    .map((prefix, index) => `${prefix}-${index}`);
 }
