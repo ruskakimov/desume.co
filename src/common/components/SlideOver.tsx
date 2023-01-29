26e9c6bfb1bb1afbf1a19238d3bdc3f90f9f4d0b
Rustem Kakimov <ruskakimov@gmail.com>
2023-01-29 14:30:07 +0600

    Change slide over to modal style

--- src/common/components/SlideOver.tsx
+++ src/common/components/SlideOver.tsx
@@ -29,10 +29,10 @@ const SlideOver: React.FC<SlideOverProps> = ({
       >
         <Transition.Child
           as={Fragment}
-          enter="ease-in-out duration-200"
+          enter="ease-out duration-300"
           enterFrom="opacity-0"
           enterTo="opacity-100"
-          leave="ease-in-out duration-500"
+          leave="ease-in duration-200"
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
@@ -42,68 +42,66 @@ const SlideOver: React.FC<SlideOverProps> = ({
         <div className="fixed inset-0" />
 
         <div className="fixed inset-0 overflow-hidden">
-          <div className="absolute inset-0 overflow-hidden">
-            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
-              <Transition.Child
-                as={Fragment}
-                enter="transform transition ease-in-out duration-200"
-                enterFrom="translate-x-full"
-                enterTo="translate-x-0"
-                leave="transform transition ease-in-out duration-500"
-                leaveFrom="translate-x-0"
-                leaveTo="translate-x-full"
-              >
-                <Dialog.Panel className="pointer-events-auto w-screen max-w-2xl">
-                  <form
-                    className="h-full"
-                    onSubmit={(e) => {
-                      e.preventDefault();
-                      onSubmit();
-                    }}
-                  >
-                    <div className="flex h-full flex-col divide-y divide-gray-200 bg-white shadow-xl">
-                      <div className="flex min-h-0 flex-1 flex-col overflow-y-scroll py-6">
-                        <div className="px-4 sm:px-6">
-                          <div className="flex items-start justify-between">
-                            <Dialog.Title className="text-lg font-medium text-gray-900">
-                              {title}
-                            </Dialog.Title>
-                            <div className="ml-3 flex h-7 items-center">
-                              <button
-                                type="button"
-                                className="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-500 no-mouse-focus-ring"
-                                onClick={onCancel}
-                              >
-                                <span className="sr-only">Close panel</span>
-                                <XMarkIcon
-                                  className="h-6 w-6"
-                                  aria-hidden="true"
-                                />
-                              </button>
-                            </div>
+          <div className="flex min-h-full items-center justify-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="pointer-events-auto w-screen max-w-2xl">
+                <form
+                  className="h-full"
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    onSubmit();
+                  }}
+                >
+                  <div className="flex h-full flex-col divide-y divide-gray-200 bg-white shadow-xl rounded-lg">
+                    <div className="flex min-h-0 flex-1 flex-col overflow-y-scroll py-6">
+                      <div className="px-4 sm:px-6">
+                        <div className="flex items-start justify-between">
+                          <Dialog.Title className="text-lg font-medium text-gray-900">
+                            {title}
+                          </Dialog.Title>
+                          <div className="ml-3 flex h-7 items-center">
+                            <button
+                              type="button"
+                              className="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-500 no-mouse-focus-ring"
+                              onClick={onCancel}
+                            >
+                              <span className="sr-only">Close panel</span>
+                              <XMarkIcon
+                                className="h-6 w-6"
+                                aria-hidden="true"
+                              />
+                            </button>
                           </div>
                         </div>
-                        <div className="relative mt-6 flex-1 px-4 sm:px-6">
-                          {children}
-                        </div>
                       </div>
-                      <div className="flex flex-shrink-0 justify-end gap-4 px-4 py-4">
-                        {onDelete && (
-                          <SecondaryButton
-                            className="text-red-600 mr-auto"
-                            onClick={onDelete}
-                          >
-                            Delete
-                          </SecondaryButton>
-                        )}
-
-                        <PrimaryButton type="submit">Save</PrimaryButton>
+                      <div className="relative mt-6 flex-1 px-4 sm:px-6">
+                        {children}
                       </div>
                     </div>
-                  </form>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
+                    <div className="flex flex-shrink-0 justify-end gap-4 px-4 py-4">
+                      {onDelete && (
+                        <SecondaryButton
+                          className="text-red-600 mr-auto"
+                          onClick={onDelete}
+                        >
+                          Delete
+                        </SecondaryButton>
+                      )}
+
+                      <PrimaryButton type="submit">Save</PrimaryButton>
+                    </div>
+                  </div>
+                </form>
+              </Dialog.Panel>
+            </Transition.Child>
           </div>
         </div>
       </Dialog>
