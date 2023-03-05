4002141bcdfa7d4a1fc85e79384d40c6dd47a60a
Rustem Kakimov <ruskakimov@gmail.com>
2023-03-05 20:18:24 +0600

    Selection step

--- src/pages/content/components/bullet-modal/useRewriteModal.tsx
+++ src/pages/content/components/bullet-modal/useRewriteModal.tsx
@@ -1,9 +1,10 @@
 import { PaperAirplaneIcon } from "@heroicons/react/24/outline";
 import classNames from "classnames";
-import React, { useLayoutEffect, useRef, useState } from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { toast } from "react-hot-toast";
 import {
   generateVariations,
+  scoreVariations,
   suggestImprovements,
 } from "../../../../api/bullet-functions";
 import TextAreaField from "../../../../common/components/fields/TextAreaField";
@@ -157,42 +158,6 @@ export default function useRewriteModal(): [OpenRewriteModal, React.ReactNode] {
     </div>
   );
 
-  const selectStepUi = (
-    <div className="space-y-6">
-      {/* <AiTextBubble>Hold on, I am scoring your submissions...</AiTextBubble> */}
-      <AiTextBubble>Here is how I would rank them.</AiTextBubble>
-
-      <div className="space-y-4">
-        {variants.map((text, index) => (
-          <div
-            className={classNames(
-              "p-4 border rounded-md flex gap-3 items-center cursor-pointer",
-              {
-                "ring-2 ring-gray-600": text === selectedVariant,
-                "opacity-50":
-                  text !== selectedVariant && selectedVariant !== "",
-              }
-            )}
-            onClick={() => setSelectedVariant(text)}
-          >
-            <div
-              className={classNames(
-                "h-7 w-7 rounded-full flex-shrink-0 flex justify-center items-center",
-                {
-                  "bg-neutral-300": index > 0,
-                  "bg-amber-400": index === 0,
-                }
-              )}
-            >
-              <span className="text-white font-bold">{index + 1}</span>
-            </div>
-            <span className="text-sm">{text}</span>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-
   return [
     (bullet) =>
       new Promise((resolve, reject) => openModal(bullet, resolve, reject)),
@@ -207,10 +172,91 @@ export default function useRewriteModal(): [OpenRewriteModal, React.ReactNode] {
         step === "generate-variants" ? generateStepButtons : selectStepButtons
       }
     >
-      {step === "generate-variants" ? generateStepUi : selectStepUi}
+      {step === "generate-variants" ? (
+        generateStepUi
+      ) : (
+        <SelectionStep
+          variants={variants}
+          selected={selectedVariant}
+          onChange={setSelectedVariant}
+        />
+      )}
     </Modal>,
   ];
 }
+
+const SelectionStep: React.FC<{
+  variants: string[];
+  selected: string;
+  onChange: (selected: string) => void;
+}> = (props) => {
+  const [scoredVariants, setScoredVariants] = useState<
+    { variant: string; score: number }[] | null
+  >(null);
+
+  useEffect(() => {
+    scoreVariations({ variations: props.variants })
+      .then((res) => {
+        const scored = res.data.scores
+          .map((score, index) => ({
+            variant: props.variants[index],
+            score,
+          }))
+          .sort((a, b) => b.score - a.score);
+        setScoredVariants(scored);
+      })
+      .catch((e) => {
+        console.error(e);
+        toast.error("Scoring failed.");
+      });
+  }, [props.variants]);
+
+  const isLoading = scoredVariants === null;
+
+  return (
+    <div className="space-y-6">
+      <AiTextBubble
+        children={
+          isLoading
+            ? "Hold on, I am scoring your submissions..."
+            : "Done! Here is how I would score your submissions. Now, you need to pick the one you want to use in your resume."
+        }
+      />
+
+      <div className="space-y-4">
+        {scoredVariants?.map(({ score, variant }, index) => (
+          <div
+            className={classNames(
+              "p-4 border rounded-md flex gap-3 items-center cursor-pointer",
+              {
+                "ring-2 ring-gray-600": variant === props.selected,
+                "opacity-50":
+                  variant !== props.selected && props.selected !== "",
+              }
+            )}
+            onClick={() => props.onChange(variant)}
+          >
+            <div
+              className={classNames(
+                "h-7 w-7 rounded-full flex-shrink-0 flex justify-center items-center",
+                {
+                  "bg-emerald-500": score >= 9,
+                  "bg-lime-500": score === 8 || score === 7,
+                  "bg-yellow-500": score === 6 || score === 5,
+                  "bg-orange-500": score === 4 || score === 3,
+                  "bg-red-500": score <= 2,
+                }
+              )}
+            >
+              <span className="text-white font-bold">{score}</span>
+            </div>
+            <span className="text-sm">{variant}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 const AiTextBubble: React.FC<{ children: React.ReactNode }> = (props) => {
   return (
