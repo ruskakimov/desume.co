5e7eb951854c78503570be030ac52eaa055768d8
Rustem Kakimov <ruskakimov@gmail.com>
2023-03-11 16:56:41 +0600

    Calc only included sections

--- src/pages/review/MetricCards.tsx
+++ src/pages/review/MetricCards.tsx
@@ -1,5 +1,9 @@
 import ShimmerOverlay from "../../common/components/ShimmerOverlay";
-import { Experience, Resume } from "../../common/interfaces/resume";
+import {
+  Experience,
+  Resume,
+  ResumeSectionId,
+} from "../../common/interfaces/resume";
 
 interface Props {
   resume: Resume | null;
@@ -62,53 +66,63 @@ function calcExpYears(exp: Experience): number {
 function calcResumeWordCount(resume: Resume): number {
   let total = 0;
 
-  total += calcWordCount(resume.personalDetails.fullName);
-  total += calcWordCount(resume.personalDetails.location);
-  total += calcWordCount(resume.personalDetails.title);
+  if (resumeContainsSection(resume, "personal")) {
+    total += calcWordCount(resume.personalDetails.fullName);
+    total += calcWordCount(resume.personalDetails.location);
+    total += calcWordCount(resume.personalDetails.title);
+  }
 
-  for (const skillGroup of resume.skillGroups) {
-    if (!skillGroup.included) continue;
+  if (resumeContainsSection(resume, "skills")) {
+    for (const skillGroup of resume.skillGroups) {
+      if (!skillGroup.included) continue;
 
-    total += calcWordCount(skillGroup.groupName);
+      total += calcWordCount(skillGroup.groupName);
 
-    for (const skill of skillGroup.skills) {
-      if (!skill.included) continue;
-      total += calcWordCount(skill.text);
+      for (const skill of skillGroup.skills) {
+        if (!skill.included) continue;
+        total += calcWordCount(skill.text);
+      }
     }
   }
 
-  for (const workExp of resume.workHistory) {
-    if (!workExp.included) continue;
+  if (resumeContainsSection(resume, "work")) {
+    for (const workExp of resume.workHistory) {
+      if (!workExp.included) continue;
 
-    total += calcWordCount(workExp.companyName);
-    total += calcWordCount(workExp.jobTitle);
+      total += calcWordCount(workExp.companyName);
+      total += calcWordCount(workExp.jobTitle);
 
-    for (const bullet of workExp.bulletPoints) {
-      if (!bullet.included) continue;
-      total += calcWordCount(bullet.text);
+      for (const bullet of workExp.bulletPoints) {
+        if (!bullet.included) continue;
+        total += calcWordCount(bullet.text);
+      }
     }
   }
 
-  for (const education of resume.educationHistory) {
-    if (!education.included) continue;
+  if (resumeContainsSection(resume, "education")) {
+    for (const education of resume.educationHistory) {
+      if (!education.included) continue;
 
-    total += calcWordCount(education.schoolName);
-    total += calcWordCount(education.degree);
+      total += calcWordCount(education.schoolName);
+      total += calcWordCount(education.degree);
 
-    for (const bullet of education.bulletPoints) {
-      if (!bullet.included) continue;
-      total += calcWordCount(bullet.text);
+      for (const bullet of education.bulletPoints) {
+        if (!bullet.included) continue;
+        total += calcWordCount(bullet.text);
+      }
     }
   }
 
-  for (const project of resume.projectHistory) {
-    if (!project.included) continue;
+  if (resumeContainsSection(resume, "projects")) {
+    for (const project of resume.projectHistory) {
+      if (!project.included) continue;
 
-    total += calcWordCount(project.projectName);
+      total += calcWordCount(project.projectName);
 
-    for (const bullet of project.bulletPoints) {
-      if (!bullet.included) continue;
-      total += calcWordCount(bullet.text);
+      for (const bullet of project.bulletPoints) {
+        if (!bullet.included) continue;
+        total += calcWordCount(bullet.text);
+      }
     }
   }
 
@@ -122,17 +136,23 @@ function calcWordCount(text: string): number {
 function calcBulletCount(resume: Resume): number {
   let count = 0;
 
-  count += resume.workHistory
-    .map(calcExpBulletCount)
-    .reduce((a, b) => a + b, 0);
+  if (resumeContainsSection(resume, "work")) {
+    count += resume.workHistory
+      .map(calcExpBulletCount)
+      .reduce((a, b) => a + b, 0);
+  }
 
-  count += resume.educationHistory
-    .map(calcExpBulletCount)
-    .reduce((a, b) => a + b, 0);
+  if (resumeContainsSection(resume, "education")) {
+    count += resume.educationHistory
+      .map(calcExpBulletCount)
+      .reduce((a, b) => a + b, 0);
+  }
 
-  count += resume.projectHistory
-    .map(calcExpBulletCount)
-    .reduce((a, b) => a + b, 0);
+  if (resumeContainsSection(resume, "projects")) {
+    count += resume.projectHistory
+      .map(calcExpBulletCount)
+      .reduce((a, b) => a + b, 0);
+  }
 
   return count;
 }
@@ -140,6 +160,17 @@ function calcBulletCount(resume: Resume): number {
 function calcExpBulletCount(exp: Experience): number {
   if (!exp.included) return 0;
   return exp.bulletPoints.reduce((count, b) => count + (b.included ? 1 : 0), 0);
+}
+
+function resumeContainsSection(
+  resume: Resume,
+  sectionId: ResumeSectionId
+): boolean {
+  return (
+    resume.sectionOrder.find(
+      (section) => section.id === sectionId && section.included
+    ) !== undefined
+  );
 }
 
 export default MetricCards;
