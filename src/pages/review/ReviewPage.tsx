e6b46a12956303b2a8bd69f9e2f66b2cdf194f7b
Rustem Kakimov <ruskakimov@gmail.com>
2023-03-11 15:09:42 +0600

    Add metrics cards

--- src/pages/review/ReviewPage.tsx
+++ src/pages/review/ReviewPage.tsx
@@ -1,13 +1,36 @@
 import Card from "../../common/components/Card";
+import PrimaryButton from "../../common/components/PrimaryButton";
 
-const ReviewPage: React.FC<{}> = (props) => {
+const stats = [
+  { name: "Years of Experience", stat: "5" },
+  { name: "Word Count", stat: "345" },
+  { name: "Bullet Count", stat: "24" },
+];
+
+const ReviewPage: React.FC = () => {
   return (
-    <div className="pb-8 lg:grid lg:grid-cols-[16rem_1fr] lg:gap-x-5">
-      <div className="mb-8 lg:mb-0">
-        <Card>Hello</Card>
-      </div>
+    <div>
+      <dl className="mb-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+        {stats.map((item) => (
+          <div
+            key={item.name}
+            className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6"
+          >
+            <dt className="truncate text-sm font-medium text-gray-500">
+              {item.name}
+            </dt>
+            <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">
+              {item.stat}
+            </dd>
+          </div>
+        ))}
+      </dl>
 
-      <Card>Hello2</Card>
+      <Card>
+        <div className="text-center">
+          <PrimaryButton>Start review</PrimaryButton>
+        </div>
+      </Card>
     </div>
   );
 };
