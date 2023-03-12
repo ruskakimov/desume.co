087a748a22b7bad162fe034abd4b8edcd9529674
Rustem Kakimov <ruskakimov@gmail.com>
2023-03-13 01:39:57 +0600

    Reposition button

--- src/pages/review/ReviewPage.tsx
+++ src/pages/review/ReviewPage.tsx
@@ -29,22 +29,20 @@ const dummyData = {
 
 const ReviewPage: React.FC = () => {
   const [resume] = useContextResume();
-  const [grammar, setGrammar] = useState<FixGrammarResponseData | null>(
-    dummyData
-  );
+  const [grammar, setGrammar] = useState<FixGrammarResponseData | null>(null);
 
-  // useEffect(() => {
-  //   if (resume === null) return;
+  useEffect(() => {
+    if (resume === null) return;
 
-  //   fixGrammar({
-  //     strings: getResumeStrings(resume),
-  //   })
-  //     .then((res) => {
-  //       setGrammar(res.data);
-  //       console.log(JSON.stringify(res.data));
-  //     })
-  //     .catch(console.error);
-  // }, [resume]);
+    fixGrammar({
+      strings: getResumeStrings(resume),
+    })
+      .then((res) => {
+        setGrammar(res.data);
+        console.log(JSON.stringify(res.data));
+      })
+      .catch(console.error);
+  }, [resume]);
 
   return (
     <div className="space-y-5">
@@ -55,7 +53,7 @@ const ReviewPage: React.FC = () => {
 
         <div>
           <h3 className="mb-4 px-6 text-xl font-semibold">
-            {grammar?.corrections.length} grammar/spelling corrections
+            {grammar?.corrections.length} grammar/spelling errors
           </h3>
 
           <div className="space-y-6">
@@ -63,12 +61,13 @@ const ReviewPage: React.FC = () => {
               const [wrongRich, fixedRich] = buildRichDiff(wrong, fixed);
 
               return (
-                <div className="flex py-4 px-6 bg-neutral-50 items-center">
-                  <div className="flex flex-col gap-2 text-left">
+                <div className="flex py-4 px-6 bg-neutral-50 items-center gap-6">
+                  <SecondaryButton>Fix</SecondaryButton>
+
+                  <div className="flex-grow flex flex-col gap-2 text-left">
                     <div className="text-gray-400">{wrongRich}</div>
                     <div className="bg-neutral-50">{fixedRich}</div>
                   </div>
-                  <SecondaryButton className="ml-auto">Apply</SecondaryButton>
                 </div>
               );
             })}
