e2e0ea4c223feaec3229bfd707e84859dd17a261
Rustem Kakimov <ruskakimov@gmail.com>
2023-03-18 12:57:43 +0600

    Remove reject

--- src/pages/review/ReviewPage.tsx
+++ src/pages/review/ReviewPage.tsx
@@ -1,32 +1,11 @@
-import classNames from "classnames";
 import { useEffect, useState } from "react";
 import { fixGrammar, FixGrammarResponseData } from "../../api/review-functions";
 import { useContextResume } from "../../AppShell";
 import Card from "../../common/components/Card";
-import PrimaryButton from "../../common/components/PrimaryButton";
 import SecondaryButton from "../../common/components/SecondaryButton";
 import { buildRichDiff } from "../../common/functions/build-rich-diff";
 import { getResumeStrings } from "../../common/functions/resume-utils";
 import MetricCards from "./MetricCards";
-
-const dummyData = {
-  corrections: [
-    {
-      fixed: "Washed the dog with dog shampoo. The dog is happy.",
-      wrong: "Washed my dog with dog shampoo. Dog is happy.",
-    },
-    {
-      fixed:
-        "Feeding a dog through a sky full of lightning was successfully completed.",
-      wrong:
-        "Task of a feeding dog through sky full of lightings was successfully completed.",
-    },
-    {
-      fixed: "Blah blah blah blah blah blah blah blah blah blah blah.",
-      wrong: "Blah blah blah blah blah blah blah blah blah blah blah.",
-    },
-  ],
-};
 
 const ReviewPage: React.FC = () => {
   const [resume] = useContextResume();
@@ -66,14 +45,12 @@ const ReviewPage: React.FC = () => {
 
               return (
                 <div className="flex py-8 px-6 bg-neutral-50 items-center gap-6">
-                  <PrimaryButton>Apply</PrimaryButton>
+                  <SecondaryButton>Apply</SecondaryButton>
 
                   <div className="flex-grow flex flex-col gap-2 text-left">
                     <div className="text-gray-400">{wrongRich}</div>
                     <div className="font-medium">{fixedRich}</div>
                   </div>
-
-                  <SecondaryButton>Reject</SecondaryButton>
                 </div>
               );
             })}
