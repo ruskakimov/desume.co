9f4e7c6aac68030922883fd42658fd75d6d1c011
Rustem Kakimov <ruskakimov@gmail.com>
2023-03-31 21:56:33 +0600

    Resize

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -21,7 +21,7 @@ const LoginPage: React.FC<{ isSignup?: boolean }> = ({ isSignup = false }) => {
   return (
     <>
       <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
-        <img className="mx-auto h-12 w-auto" src={logo} alt="PDFEGG" />
+        <img className="mx-auto h-8 w-auto" src={logo} alt="" />
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
